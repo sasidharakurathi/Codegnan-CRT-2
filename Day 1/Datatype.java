0f002978d620fb2d32701a85d90ed116970298ca
AKURATHI SASIDHAR <22kt1a0595@gmail.com>
2025-09-08 11:31:54 +0530

    Day 1 - Program 1

---
+++ Day 1/Datatype.java
@@ -0,0 +1,47 @@
+
+
+class Datatype {
+    public static void main(String[] args) {
+        System.out.println("\nByte: ");
+        System.out.println(Byte.SIZE);
+        System.out.println(Byte.SIZE/8);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        
+        System.out.println("\nShort: ");
+        System.out.println(Short.SIZE);
+        System.out.println(Short.SIZE/8);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        
+        System.out.println("\nInteger: ");
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.SIZE/8);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        
+        System.out.println("\nLong: ");
+        System.out.println(Long.SIZE);
+        System.out.println(Long.SIZE/8);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        
+        System.out.println("\nFloat: ");
+        System.out.println(Float.SIZE);
+        System.out.println(Float.SIZE/8);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+
+        System.out.println("\nDouble: ");
+        System.out.println(Double.SIZE);
+        System.out.println(Double.SIZE/8);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+
+        System.out.println("\nCharacter: ");
+        System.out.println(Character.SIZE);
+        System.out.println(Character.SIZE/8);
+        System.out.println(Character.MIN_VALUE);
+        System.out.println(Character.MAX_VALUE);
+    }
+}
